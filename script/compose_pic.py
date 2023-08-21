75a76f02bc5eb6c596a83c450da7a035a654e398
pickxiguapi <1084612722@qq.com>
2023-08-21 19:04:34 +0800

    Update Site

---
+++ script/compose_pic.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project ：rl-lab 
+@File    ：compose_pic.py
+@Author  ：Yifu Yuan
+@Date    ：2023/8/21
+"""
+from PIL import Image
+import os
+
+folder_path = r"E:\rl-lab\assets\image\team"
+for filename in os.listdir(folder_path):
+    if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg'):
+        try:
+            image = Image.open(os.path.join(folder_path, filename))
+    
+            if os.path.getsize(os.path.join(folder_path, filename)) > 512 * 512:
+                image = image.resize((int(image.size[0]*0.5), int(image.size[1]*0.5)))
+                image.save(os.path.join(folder_path, filename))
+        except:
+            print(filename)
+
+print("Finish")
