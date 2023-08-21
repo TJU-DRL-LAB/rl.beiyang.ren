e5045e523702ddc350dd616da5439bcf4d09df24
pickxiguapi <1084612722@qq.com>
2023-08-21 19:33:04 +0800

    Update Site

--- script/transform_xlsx_to_json.py
+++ script/transform_xlsx_to_json.py
@@ -16,9 +16,9 @@ total_json = []
 df = pd.read_excel("faculty_info.xlsx")
 result = {"type": "Faculty", "item": []}
 for index, row in df.iterrows():
-    img_path = f'assets/image/team/{row["姓名"]}.jpg'
+    img_path = f'/assets/image/team/{row["姓名"]}.jpg'
     if not os.path.exists("../" + img_path):
-        img_path = f'assets/image/team/{row["姓名"]}.png'
+        img_path = f'/assets/image/team/{row["姓名"]}.png'
     link = row["个人主页"]
     item = {"name": row["姓名"],
             "desc": row["职位"], "img": img_path,
@@ -37,9 +37,9 @@ for type_index, type in type_dict.items():
     result = {"type": type, "item": []}
     for index, row in df.iterrows():
         if row["身份"] == type_index:
-            img_path = f'assets/image/team/{row["姓名"]}.jpg'
+            img_path = f'/assets/image/team/{row["姓名"]}.jpg'
             if not os.path.exists("../" + img_path):
-                img_path = f'assets/image/team/{row["姓名"]}.png'
+                img_path = f'/assets/image/team/{row["姓名"]}.png'
             if row["个人主页链接"] == "(空)":
                 link = ''
             else:
@@ -53,14 +53,36 @@ for type_index, type in type_dict.items():
     
     total_json.append(result)
 
+
 # -----------------------------Alumni-----------------------------------
-result = {"type": "Alumni", "item": []}
+result = {"type": "Alumni (Ph.D.)", "item": []}
+loc_dict = {1: "Master", 2: "Ph.D."}
+df = pd.read_excel("alumni_info_phd.xlsx")
+for index, row in df.iterrows():
+    img_path = f'/assets/image/team/{row["姓名"]}.jpg'
+    if not os.path.exists("../" + img_path):
+        img_path = f'/assets/image/team/{row["姓名"]}.png'
+    if row["个人主页链接"] == "(空)":
+        link = ''
+    else:
+        link = row["个人主页链接"]
+    item = {"name": row["姓名"], "desc": f"Graduated with a {loc_dict[row['身份']]} "
+                                         f"degree in {row['毕业年份']}, {row['毕业去向']}", "img": img_path,
+            "link": link, "area": "Research Area: " + row["研究方向（英文）"]}
+    result["item"].append(item)
+print(result)
+
+total_json.append(result)
+
+
+# -----------------------------Alumni-----------------------------------
+result = {"type": "Alumni (Master)", "item": []}
 loc_dict = {1: "Master", 2: "Ph.D."}
 df = pd.read_excel("alumni_info.xlsx")
 for index, row in df.iterrows():
-    img_path = f'assets/image/team/{row["姓名"]}.jpg'
+    img_path = f'/assets/image/team/{row["姓名"]}.jpg'
     if not os.path.exists("../" + img_path):
-        img_path = f'assets/image/team/{row["姓名"]}.png'
+        img_path = f'/assets/image/team/{row["姓名"]}.png'
     if row["个人主页链接"] == "(空)":
         link = ''
     else:
